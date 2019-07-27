8f52f767e57a31ed9485de2ae53b507423c7f4ef
Youthhub <>
2019-07-27 21:07:26 +0900

    미니게임 시작!

--- src/App.js
+++ src/App.js
@@ -8,9 +8,7 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <p>자, 미니게임을 시작해봅시다!!</p>
           <a
             className="App-link"
             href="http://www.vistavielab.com"
