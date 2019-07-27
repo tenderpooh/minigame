608191976770f803fe9932f71ab989ac0baf973d
Youthhub <>
2019-07-27 20:55:51 +0900

    example

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
 import logo from "./logo.svg";
-import Header from "./Header.js";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <Header />
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
