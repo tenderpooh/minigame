82eee9112d7e73a3534ac8e484c4c77a16e18891
김소년 <kim.boyhood@gmail.com>
2019-07-29 14:23:55 +0900

    게이머보드를 추가했습니다.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
+import Header from "./Header";
+import Gameboard from "./gameboard";
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <>
+      <Header />
+      <Gameboard />
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
