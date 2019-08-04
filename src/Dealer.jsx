5277f968a4ec85190161b7f332668602c2b2d8d5
Youthhub <>
2019-08-04 12:13:23 +0900

    딜러 컴포넌트 조건문 사용

--- src/Dealer.jsx
+++ src/Dealer.jsx
@@ -4,17 +4,31 @@ import { Input, Button, Container, Row } from "reactstrap";
 const Dealer = () => {
   const [Code, setCode] = useState("");
   const [Score, setScore] = useState("");
+  let playerName = "";
+  if (Code === "1111") {
+    playerName = "김소년";
+  } else {
+    playerName = "빠울루";
+  }
+  if (Code.length === 4) {
+    let scoreInput = document.getElementById("scoreInput");
+    scoreInput.focus();
+  }
   return (
     <Container>
       <div className="d-flex flex-column align-items-center justify-content-center vh-100">
         <Input
           onChange={e => setCode(e.target.value)}
           value={Code}
+          placeholder="유저코드"
           style={{ width: "80%" }}
         />
+        <div>사용자 이름 : {playerName}</div>
         <Input
+          id="scoreInput"
           onChange={e => setScore(e.target.value)}
           value={Score}
+          placeholder="점수"
           style={{ width: "80%" }}
         />
         <Button
